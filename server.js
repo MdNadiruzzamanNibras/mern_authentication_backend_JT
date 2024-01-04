56c5b00b2085e70a2bebc9518d04caf3a84f6284
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-01-04 18:47:07 +0600

    db connect

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
@@ -8,6 +8,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+let uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uf4d38c.mongodb.net/?retryWrites=true&w=majority`
+
+// connectDB is a function that connect mongodb database
+async function connectDB () {
+    try{
+      await mongoose.connect(uri)
+      console.log("connect db");
+    }
+    catch{
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+connectDB();
 // get root path and send a message
 app.get('/', (req, res) => {
   res.send('Hello World!')
